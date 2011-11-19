dd6eb34283ca34bcf386f3eac22b82a89b117c6e
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-11-19 12:42:36 -0430

    Genetico.java: main.

---
+++ Genetico.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genetico;
+
+/**
+ *
+ * @author heraclio
+ */
+public class Genetico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
