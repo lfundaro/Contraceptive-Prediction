df74cb6423afd1e2337e6b9a244466dae3d540e0
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-11-20 10:02:47 -0430

    Calling file parser.
    Signed-off-by: Lorenzo Fundaró García <lorenzofundaro@yahoo.com>

--- Genetico.java
+++ Genetico.java
@@ -1,19 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package genetico;
 
-/**
- *
- * @author heraclio
- */
+import java.util.ArrayList;
+
 public class Genetico {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            Parser p = new Parser(args[0]);
+            ArrayList<int[]> trainingData = p.go();
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Especificar archivo de entrada: "+e);
+        }
+        
     }
 }
