bd8ae394575b42e4866ee0ff425b869970a7acd2
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-11-20 10:03:37 -0430

    Parser.java: new class. It allows to parse data files.
    Signed-off-by: Lorenzo Fundaró García <lorenzofundaro@yahoo.com>

---
+++ Parser.java
@@ -0,0 +1,201 @@
+package genetico;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lorenzo Fundaró <lorenzofundaro [at] yahoo.com>
+ */
+public class Parser {
+    public String filePath;
+    public final int REP_SIZE = 36;
+    
+    public Parser(String fname) {
+        filePath = fname;
+    }
+    
+    public ArrayList<int[]> go() {
+        ArrayList<int[]> pool = new ArrayList<int[]>();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(filePath));
+            String nextLine;
+            String[] tokenized;
+            int[] ex;
+            int tmp;
+            int maxIndex;
+
+            while((nextLine = in.readLine()) != null) {
+                maxIndex = 0;
+                tokenized = nextLine.split(",");
+                ex = new int[REP_SIZE];
+                init(ex);
+                // Edad de la mujer
+                tmp = Integer.parseInt(tokenized[0]);
+                maxIndex = calcAge(ex, tmp, maxIndex);
+                // Educación de la mujer
+                tmp = Integer.parseInt(tokenized[1]);
+                maxIndex = calCat(ex, tmp, maxIndex);
+                // Educación del esposo
+                tmp = Integer.parseInt(tokenized[2]);
+                maxIndex = calCat(ex, tmp, maxIndex);
+                // Número de hijos
+                tmp = Integer.parseInt(tokenized[3]);
+                maxIndex = numChildren(ex, tmp, maxIndex);
+                // Religión de la mujer
+                tmp = Integer.parseInt(tokenized[4]);
+                maxIndex = calRel(ex, tmp, maxIndex);
+                // Madre en situación de trabajo ?
+                tmp = Integer.parseInt(tokenized[5]);
+                maxIndex = calWorking(ex, tmp, maxIndex);
+                // Ocupación del esposo
+                tmp = Integer.parseInt(tokenized[6]);
+                maxIndex = calCat(ex, tmp, maxIndex);
+                // Estándar de vida
+                tmp = Integer.parseInt(tokenized[7]);
+                maxIndex = calCat(ex, tmp, maxIndex);     
+                // Exposición al ambiente
+                tmp = Integer.parseInt(tokenized[8]);
+                maxIndex = calMediaExp(ex, tmp, maxIndex);  
+                // Método anticonceptivo
+                tmp = Integer.parseInt(tokenized[9]);
+                ex[maxIndex] = tmp;        
+                pool.add(ex);
+            }
+        }
+        catch(FileNotFoundException fnf) {
+            System.out.println("No se pudo encontrar el archivo: "+filePath);
+        }
+        catch(IOException e) {
+            System.out.println("Error de E/S: "+e);
+        }
+        return pool;
+    }
+    
+    private void init(int[] ex) {
+        for(int i = 0; i < ex.length; i++)
+            ex[i] = 0;
+    }
+    
+    private int calRel(int[] ex, int val, int maxIndex) {
+        if (val == 1)    // Islam
+            ex[maxIndex] = 1;
+        else             // No-Islam
+            ex[maxIndex + 1] = 1;
+        
+        return maxIndex + 2;
+    }
+    
+    private int calWorking(int[] ex, int val, int maxIndex) {
+        if (val == 0)    // YES
+            ex[maxIndex] = 1;
+        else             // NO
+            ex[maxIndex + 1] = 1;
+        
+        return maxIndex + 2;
+    }
+    
+    private int calMediaExp(int[] ex, int val, int maxIndex) {
+        if (val == 0)    // Good
+            ex[maxIndex] = 1;
+        else             // Not good
+            ex[maxIndex + 1] = 1;
+        
+        return maxIndex + 2;
+    }
+    
+    private int numChildren(int[] ex, int nc, int maxIndex) {
+        if (nc == 0) 
+            ex[maxIndex] = 1;
+        else if (nc == 1) 
+            ex[maxIndex + 1] = 1;
+        else if (nc == 2) 
+            ex[maxIndex + 2] = 1;
+        else if (nc == 3)
+            ex[maxIndex + 3] = 1;
+        else if (nc == 4) 
+            ex[maxIndex + 4] = 1;
+        else   // Para personas con hijos >= 5.
+            ex[maxIndex + 5] = 1;
+        
+        return maxIndex + 6;
+    }
+    
+    private int calCat(int[] ex, int level, int maxIndex) {
+        if (level == 1) 
+            ex[maxIndex] = 1;
+        else if (level == 2) 
+            ex[maxIndex + 1] = 1;
+        else if (level == 3)
+            ex[maxIndex + 2] = 1;
+        else 
+            ex[maxIndex + 3] = 1;
+        
+        return maxIndex + 4;
+    }
+    
+    private int calcAge(int[] ex, int age, int maxIndex) {   
+        if (age >= 15 && age <= 20)
+            ex[maxIndex] = 1;
+        else if (age > 20 && age <= 25)
+            ex[maxIndex + 1] = 1;
+        else if (age > 25 && age <= 30)
+            ex[maxIndex + 2] = 1;
+        else if (age > 30 && age <= 35)
+            ex[maxIndex + 3] = 1;
+        else if (age > 35 && age <= 40)
+            ex[maxIndex + 4] = 1;
+        else if (age > 40 && age <= 45)
+            ex[maxIndex + 5] = 1;
+        else     // age > 45 && age <= 50
+            ex[maxIndex + 6] = 1;
+        
+        return maxIndex + 7;  // Máximo índice que utiliza esta función
+    }
+    
+    public void test(int[] test) {
+        System.out.println("Edad");
+        for(int i = 0; i < 7; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+        System.out.println("Educación esposa");
+        for(int i = 7; i < 11; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+        System.out.println("Educación del esposo");
+        for(int i = 11; i < 15; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+        System.out.println("Número de hijos");
+        for(int i = 15; i < 21; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+        System.out.println("Religión de la esposa");
+        for(int i = 21; i < 23; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+        System.out.println("Situación de trabajo de la esposa");
+        for(int i = 23; i < 25; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+        System.out.println("Ocupación del marido");
+        for(int i = 25; i < 29; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+        System.out.println("Estándar de vida");
+        for(int i = 29; i < 33; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+        System.out.println("Exposición al ambiente");
+        for(int i = 33; i < 35; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+        System.out.println("Método anticonceptivo");
+        for(int i = 35; i < 36; i++)
+            System.out.print(test[i]);
+        System.out.println("");
+    }
+}
