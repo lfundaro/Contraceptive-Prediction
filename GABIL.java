182d8340915f2f7b43546373679c311973869f3b
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-11-20 19:34:12 -0430

    GABIL.java: initPop, threshold element, genField, 	fitness, testAgainstTS, matchRulePortion, rouletteSelection, 	go. 	Pair.java: new class.

--- GABIL.java
+++ GABIL.java
@@ -2,6 +2,7 @@
 package genetico;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 /**
@@ -13,15 +14,18 @@ public class GABIL {
     int p;     // tamaño de población.
     double r;   // fracción de la población que será reemplazada.
     double m;   // Tasa de mutación.
+    double fitness_threshold;
     
-    public GABIL(ArrayList<int[]> training, int p, double r, double m) {
+    public GABIL(ArrayList<int[]> training, int p, double r, double m, 
+            double fthld) {
         trainingData = training;
         this.p = p;
         this.r = r;
         this.m = m;
+        fitness_threshold = fthld;
     }
     
-    public ArrayList<int[]> initPop() {
+    private ArrayList<int[]> initPop() {
         // Generación de una hipótesis aleatoria
         ArrayList<int[]> pool = new ArrayList<int[]>(p);
         Random gen = new Random();
@@ -124,8 +128,92 @@ public class GABIL {
             ex[i] = 0;
     }
     
+    private double[] fitness(ArrayList<int[]> pool) {
+        Iterator<int[]> it = pool.iterator();
+        double[] ftn = new double[pool.size()];
+        int cnt = 0;
+        double overall = 0.0;
+        while (it.hasNext()) {
+            int[] hyp = it.next();
+            int nrules = hyp.length / Parser.REP_SIZE;
+            int i = 0;
+            int acc = 0;
+            int index = 0;
+            // Ṕrobar cada regla contra el training set.
+            while (i < nrules) {
+                acc += testAgainstTS(hyp, index);
+                i++;
+                index += Parser.REP_SIZE;
+            }
+            // Calcular fitness overall de la hipótesis
+            overall = ((double) acc) / ((double) trainingData.size());
+            // Asignar a la i-ésima hipótesis su valor fitness
+            ftn[cnt] = overall;
+            cnt++;
+        }
+        return ftn;
+    }
+    
+    private int testAgainstTS(int[] hyp, int l) {
+        Iterator<int[]> it = trainingData.iterator();
+        int[] seq = {Parser.AGE_SIZE, Parser.CAT_SIZE, Parser.CAT_SIZE,
+        Parser.NCHILD_SIZE, Parser.REL_SIZE, Parser.WORK_SIZE, Parser.CAT_SIZE,
+        Parser.CAT_SIZE, Parser.MEX_SIZE, Parser.MTH_SIZE};
+        int classified = 0;
+        while (it.hasNext()) {
+            int[] example = it.next();
+            int field = 0;
+            int index = l;
+            boolean testOK = true;
+            while (testOK && field < Parser.FIELDS) {
+                testOK &= matchRulePortion(hyp, example, index, 
+                        index + seq[field]);
+                index += seq[field];
+                field++;
+            }
+            if (testOK) {classified++;}
+        }
+        return classified;
+    }
+    
+    private boolean matchRulePortion(int[] hyp, int[] example, int l, int h) {
+        int valid = 0;
+        for(int i = l; i < h; i++) {
+            valid |= hyp[i] & example[i];
+        }
+        return valid == 1;
+    }
+    
+    private double max(double[] ftn) {
+        double h = 0.0;
+        for(int i = 0; i < ftn.length; i++) {
+            h = Math.max(h, ftn[i]);
+        }
+        return r;
+    }
+    
+    private ArrayList<int[]> rouletteSelection(int prop, double[] ftn, 
+            ArrayList<int[]> population) {
+        // Suma de todos los fitness
+        double sum = 0.0;
+        for(int i = 0; i < ftn.length; i++)
+            sum += ftn[i];
+        // Probabilidad de cada hipótesis
+        double[] pr = new double[ftn.length];
+        for(int i = 0; i < pr.length; i++) 
+            pr[i] = ftn[i] / sum;
+        ArrayList<Pair> prs = new ArrayList<Pair>(population.size());
+    }
+    
     public int[] go() {
         int[] finalHyp = new int[20];
+        ArrayList<int[]> population = initPop();
+        double[] ftn = fitness(population);
+        while (max(ftn) < fitness_threshold) {
+            // Crear nueva generación
+            int prop = (int) ((1-r)*p); 
+            ArrayList<int[]> Ps = rouletteSelection(prop, ftn, population);
+        }
         return finalHyp;
     }
 }
---
+++ Pair.java
@@ -0,0 +1,26 @@
+
+package genetico;
+
+/**
+ *
+ * @author Lorenzo Fundaró <lorenzofundaro [at] yahoo.com>
+ */
+public class Pair {
+    int left;
+    double right;
+    
+    public Pair(int left, double right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public double getRight() {
+        return right;
+    }
+    
+    
+}
