dc950f28c5ea2e2f0dd45885fa6a580e43cfabce
Jose <josej30@gmail.com>
2011-11-21 00:24:36 -0430

    Complete crossover and a function that validates hypothesis

--- Genetico.java
+++ Genetico.java
@@ -12,6 +12,11 @@ public class Genetico {
             Parser p = new Parser(args[0]);
             ArrayList<int[]> trainingData = p.go();
             GABIL g = new GABIL(trainingData, 10, 0.5, 0.0001, 1.0);
+
+            int [] hyp = {0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,};
+            Boolean xxx = g.validHyp(hyp);
+            if (xxx) System.out.println("RIGHT"); else System.out.println("WRONG");
+
         }
         catch(ArrayIndexOutOfBoundsException e) {
             System.out.println("Especificar archivo de entrada: "+e);
